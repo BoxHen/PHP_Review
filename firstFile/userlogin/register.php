586f0c8f16b7604bbbd5f8795302e793de910194
Henry <Hchen101@binghamton.edu>
2018-08-09 21:56:19 -0400

    character length

--- firstFile/userlogin/register.php
+++ firstFile/userlogin/register.php
@@ -11,25 +11,29 @@
       $lastname =  mysqli_real_escape_string($conn, $_POST['lastname']);
       $password_hashed = MD5($password);
       if(!empty($_POST['username'])&&!empty($_POST['password'])&&!empty($_POST['password2'])&&!empty($_POST['firstname'])&&!empty($_POST['lastname'])){
-        if($password==$password2){ // checks if the passwords match
-          $sql="SELECT `username` FROM `users` WHERE `username`='$username'";
-          $result = $conn->query($sql);
-          if($result->num_rows >= 1){ // username is NOT unique
-            while($row = $result->fetch_assoc()){
-              echo 'Please try another username. The username, '.$row['username'].' is already taken';
+        if(strlen($username>30||strlen($firstname>30)||strlen($lastname>30))){
+          echo 'field(s) are too long. Please keep below 30 characters!';
+        }else{
+          if($password==$password2){ // checks if the passwords match
+            $sql="SELECT `username` FROM `users` WHERE `username`='$username'";
+            $result = $conn->query($sql);
+            if($result->num_rows >= 1){ // username is NOT unique
+              while($row = $result->fetch_assoc()){
+                echo 'Please try another username. The username, '.$row['username'].' is already taken';
+              }
+            }else{ // username is unique
+              $sql = "INSERT INTO `users`(`username`, `password`, `first name`, `last name`) VALUES ('{$username}','{$password_hashed}','{$firstname}','{$lastname}')";
+              //$sql = "INSERT INTO `users`(`username`, `password`, `first name`, `last name`) VALUES ('".$username."','".$password_hashed."','".$firstname."','".$lastname."')";
+              if($conn->query($sql)){
+                //echo 'Welcome new user!'; // we will direct them to new page so they cannot reload and keep registering
+                header('Location: register_success.php');
+              }else{
+                echo 'We could not sign you up at this time. Try again later.';
+              }
             }
-          }else{ // username is unique
-            $sql = "INSERT INTO `users`(`username`, `password`, `first name`, `last name`) VALUES ('{$username}','{$password_hashed}','{$firstname}','{$lastname}')";
-            //$sql = "INSERT INTO `users`(`username`, `password`, `first name`, `last name`) VALUES ('".$username."','".$password_hashed."','".$firstname."','".$lastname."')";
-            if($conn->query($sql)){
-              //echo 'Welcome new user!'; // we will direct them to new page so they cannot reload and keep registering
-              header('Location: register_success.php');
-            }else{
-              echo 'We could not sign you up at this time. Try again later.';
-            }
+          }else{ // passwords do not match
+            echo 'passwords do not match. Please re-enter password.';
           }
-        }else{ // passwords do not match
-          echo 'passwords do not match. Please re-enter password.';
         }
       }else{
         echo 'All fields are required!';
@@ -38,11 +42,11 @@
 ?>
 
 <form action="register.php" method="POST">
-  Username:<br> <input type="text" name="username" value="<?php if(isset($username)){echo $username;}?>"><br>
+  Username:<br> <input type="text" name="username" maxlength="30" value="<?php if(isset($username)){echo $username;}?>"><br>
   Password:<br> <input type="password" name="password"><br>
   Re-enter Password:<br> <input type="password" name="password2"><br>
-  First Name:<br> <input type="text" name="firstname" value="<?php if(isset($username)){echo $firstname;}?>"><br>
-  Last Name:<br> <input type="text" name="lastname" value="<?php if(isset($username)){echo $lastname;}?>"><br><br>
+  First Name:<br> <input type="text" name="firstname" maxlength="30" value="<?php if(isset($username)){echo $firstname;}?>"><br>
+  Last Name:<br> <input type="text" name="lastname" maxlength="30" value="<?php if(isset($username)){echo $lastname;}?>"><br><br>
   <input type="submit" name="submit" value="Submit"><br>
 </form>
 

fcfd0d566655de4d493f533ccbf1d5b11a6b1cea
Henry <Hchen101@binghamton.edu>
2018-08-14 00:28:53 -0400

    string to image with forms(complete)

--- firstFile/Email_StringToImage/generate.php
+++ firstFile/Email_StringToImage/generate.php
@@ -1,21 +1,33 @@
 <?php
 header('Content-type: image/jpeg');
-require 'connect.php';
+//require 'connect.php'; // why can't I use require????
+//==============================================================================
+$conn_error = 'Could Not Connect';
+$mysql_host = 'localhost';
+$mysql_user = 'root';
+$mysql_pass = '';
+$mysql_db = 'user_login';
 
+$conn = new mysqli($mysql_host, $mysql_user, $mysql_pass,$mysql_db);
+if($conn->connect_error){
+  die($conn_error);
+}
+//==============================================================================
 //$email = 'something@something.com'; //make this dynamic
 if(isset($_GET['id'])){
   $id = $_GET['id'];
-  $sql = "SELECT `username` FROM `users` WHERE `id`='".mysqli_real_escape_string($id)."'";
+  $sql = "SELECT * FROM `users` WHERE `id`='".mysqli_real_escape_string($conn, $id)."'";
   $result = $conn->query($sql);
   if($result->num_rows >= 1){
     while($row=$result->fetch_assoc()){
       $email = $row['username'];
     }
+  }else{
+    $email = 'error';
   }
 }else{
   $email = 'None';
 }
-
 
 $email_length = strlen($email);
 $font_size = 4;
--- firstFile/Email_StringToImage/index.php
+++ firstFile/Email_StringToImage/index.php
@@ -2,4 +2,4 @@ Name:<br>
 Henry
 <br><br>
 Email:<br>
-<img src="generate.php" alt="">
+<img src="generate.php?id=2" alt="">

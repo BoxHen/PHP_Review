7d76dface90fd766fa9c39c7f524fcd13cb022d4
Henry <hchen101@binghamton.edu>
2018-08-22 00:40:58 -0400

    captcha done

--- firstFile/CaptchaSecurity/generate.php
+++ firstFile/CaptchaSecurity/generate.php
@@ -5,14 +5,24 @@ header('Content-type: image/jpeg');
 $text = $_SESSION['secure'];
 $font_size = 30; // set captcha font
 
-$image_width = 200; // set captcha width /height
+$image_width = 120; // set captcha width /height
 $image_height = 40;
 
 $image = imagecreate($image_width, $image_height);
 imagecolorallocate($image, 255, 255, 255); // allocate white to be used
 $text_color = imagecolorallocate($image, 0, 0, 0); //allocate black as color for text
+
+for($x=1; $x<=30; $x++){ // used to create random lines in the text(noise)
+  $x1 = rand(1, 100);
+  $x2 = rand(1, 100);
+  $y1 = rand(1, 100);
+  $y2 = rand(1, 100);
+
+  imageline($image, $x1, $y1, $x2 , $y2, $text_color);
+}
+
 //putenv('GDFONTPATH=' . realpath('.')); // Set the enviroment variable for GD
-$font = dirname(__FILE__) . '/fonts/arial.ttf';//../Windows/Fonts/
+$font = dirname(__FILE__) . '/fonts/font.ttf';//../Windows/Fonts/
 imagettftext($image, $font_size, 0, 15, 30, $text_color, $font, $text);
 //wierd bug with ttf file being in same folder, so keep ttf in separate fonts folder
 imagejpeg($image); // get to browser
--- firstFile/CaptchaSecurity/index.php
+++ firstFile/CaptchaSecurity/index.php
@@ -1,7 +1,21 @@
 <?php
 session_start();
-$_SESSION['secure'] = rand(1000,9999);
+
+if(!isset($_POST['secure'])){
+  $_SESSION['secure'] = rand(1000,9999);
+}else{
+  if($_SESSION['secure']==$_POST['secure']){
+    echo 'good'; // forward to different page
+  }else{
+    echo 'incorrect';
+    $_SESSION['secure'] = rand(1000,9999); // need to change on wrong one
+  }
+}
+
 
 ?>
 
 <img src="generate.php" alt="">
+<form action="index.php" method="POST">
+  Type what you see: <input type="text" size="6" name="secure"> <input type="submit" value="submit">
+</form>

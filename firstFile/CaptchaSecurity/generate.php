93414b27be14ae980301eaa853bfc4aa2a1b666a
Henry <hchen101@binghamton.edu>
2018-08-22 00:22:01 -0400

    imagettftext bug(we cannot keep ttf file in same directory)

--- firstFile/CaptchaSecurity/generate.php
+++ firstFile/CaptchaSecurity/generate.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-//header('Content-type: image/jpeg');
+header('Content-type: image/jpeg');
 
 $text = $_SESSION['secure'];
 $font_size = 30; // set captcha font
@@ -11,8 +11,10 @@ $image_height = 40;
 $image = imagecreate($image_width, $image_height);
 imagecolorallocate($image, 255, 255, 255); // allocate white to be used
 $text_color = imagecolorallocate($image, 0, 0, 0); //allocate black as color for text
-$font = dirname("/C:/Windows/Fonts").'arial.ttf';//../Windows/Fonts/
+//putenv('GDFONTPATH=' . realpath('.')); // Set the enviroment variable for GD
+$font = dirname(__FILE__) . '/fonts/arial.ttf';//../Windows/Fonts/
 imagettftext($image, $font_size, 0, 15, 30, $text_color, $font, $text);
+//wierd bug with ttf file being in same folder, so keep ttf in separate fonts folder
 imagejpeg($image); // get to browser
 
  ?>
